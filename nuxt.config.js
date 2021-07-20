dee37fbd932205c6ed88ff6edf3f92a6bcd9f0bd
Tuan Anh Tran Nguyen <mraky09@Tuans-MacBook-Pro.local>
2021-07-20 23:10:35 +0700

    fix: change deploy mode to server

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,27 +1,27 @@
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'workbook-factory',
+    title: "workbook-factory",
     meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      { hid: "description", name: "description", content: "" },
     ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }],
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: ['./assets/scss/colors.scss', './assets/scss/main.scss'],
+  css: ["./assets/scss/colors.scss", "./assets/scss/main.scss"],
   // You will have to add this new object if it doesn't exist already
   styleResources: {
-    scss: ['./assets/scss/*.scss'],
+    scss: ["./assets/scss/*.scss"],
   },
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
-    { src: '~/plugins/vuex-persist.js', ssr: false },
-    { src: '~/plugins/axios.js', ssr: true },
-    { src: '@/plugins/vue-awesome-swiper', mode: 'client' },
+    { src: "~/plugins/vuex-persist.js", ssr: false },
+    { src: "~/plugins/axios.js", ssr: true },
+    { src: "@/plugins/vue-awesome-swiper", mode: "client" },
   ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
@@ -30,24 +30,24 @@ export default {
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
     // https://go.nuxtjs.dev/eslint
-    '@nuxtjs/eslint-module',
+    "@nuxtjs/eslint-module",
     // https://go.nuxtjs.dev/stylelint
-    '@nuxtjs/stylelint-module',
+    "@nuxtjs/stylelint-module",
     // https://go.nuxtjs.dev/tailwindcss
-    '@nuxtjs/tailwindcss',
+    "@nuxtjs/tailwindcss",
   ],
 
   // Modules: https://go.nuxtjs.dev/config-modules
   modules: [
     // https://go.nuxtjs.dev/axios
-    '@nuxtjs/axios',
-    'cookie-universal-nuxt',
+    "@nuxtjs/axios",
+    "cookie-universal-nuxt",
     // https://go.nuxtjs.dev/pwa
-    '@nuxtjs/pwa',
-    '@nuxtjs/style-resources',
+    "@nuxtjs/pwa",
+    "@nuxtjs/style-resources",
     // https://vue.chakra-ui.com/with-nuxt
-    '@chakra-ui/nuxt',
-    '@nuxtjs/emotion',
+    "@chakra-ui/nuxt",
+    "@nuxtjs/emotion",
   ],
   /**
    * Add extend the plugin options under the `chakra` key.
@@ -95,22 +95,23 @@ export default {
   },
   publicRuntimeConfig: {
     axios: {
-      baseURL: 'https://workbookfactory-api.herokuapp.com',
+      baseURL: "https://workbookfactory-api.herokuapp.com",
     },
   },
 
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
-    baseURL: 'https://workbookfactory-api.herokuapp.com',
+    baseURL: "https://workbookfactory-api.herokuapp.com",
   },
 
   // PWA module configuration: https://go.nuxtjs.dev/pwa
   pwa: {
     manifest: {
-      lang: 'en',
+      lang: "en",
     },
   },
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {},
-}
+  target: "server",
+};
